94564406fa82414f0c2d23d3194a7bc99d053ddc
Houcine DG <digrari756@gmail.com>
2023-11-22 23:54:25 +0100

    adding my current first even django project

---
+++ first_project/first_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('first_app/', views.first_app, name='first_app'),
+    path('second_view/', views.second_view, name = 'second_view'),
+    path('first_app/details/<int:id>', views.details, name='details')
+]
---
+++ first_project/first_app/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Person
+
+# Create your views here.
+
+def first_app(request): #first view
+    template = loader.get_template('firstHtml.html') #this view is related to this template
+    person_data = Person.objects.all().values()
+    context = {
+        'person_data' : person_data
+    }
+    return HttpResponse(template.render(context, request))
+
+def second_view(request):# but this view is rendering html directly 
+    content_page = "Hello world from another view! <span style='color: red;'>yes</span>" 
+    return HttpResponse(content_page)
+
+def details(request, id):
+    template = loader.get_template('details.html') # the details view
+    person_item = Person.objects.get(id=id)
+    context = {
+        'person_item': person_item
+    }
+    return HttpResponse(template.render(context, request))

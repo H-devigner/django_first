348fb96cf9a28c80176340856c7eca41fc9fc0ea
Houcine DG <digrari756@gmail.com>
2023-11-23 17:49:21 +0100

    added a 404 page for false urls

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5&35u10tr8yfigec%u7yp^*4p39!vl5sh7h-+r0#tol6ds#@4@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
